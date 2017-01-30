c00374b23c161e7d237d11a80624579aedeca180
mlinglhl <minhung.ling@gmail.com>
2017-01-30 12:11:39 -0500

    Initial Commit

---
+++ 20170130 iPhone Images/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  20170130 iPhone Images
+//
+//  Created by Minhung Ling on 2017-01-30.
+//  Copyright © 2017 Minhung Ling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 20170130 iPhone Images/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  20170130 iPhone Images
+//
+//  Created by Minhung Ling on 2017-01-30.
+//  Copyright © 2017 Minhung Ling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
