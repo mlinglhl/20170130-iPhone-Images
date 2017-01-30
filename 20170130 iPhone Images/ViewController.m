66fea0ae1447eacf2a01129001251e0821638e73
mlinglhl <minhung.ling@gmail.com>
2017-01-30 12:57:03 -0500

    Final commit

--- 20170130 iPhone Images/ViewController.m
+++ 20170130 iPhone Images/ViewController.m
@@ -9,21 +9,69 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UIImageView *iPhoneImageView;
+@property NSArray *phoneURLArray;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.phoneURLArray = @[ [NSURL URLWithString:@"http://imgur.com/bktnImE.png"],
+                            [NSURL URLWithString:@"http://imgur.com/zdwdenZ.png"],
+                            [NSURL URLWithString:@"http://imgur.com/CoQ8aNl.png"],
+                            [NSURL URLWithString:@"http://imgur.com/2vQtZBb.png"],
+                            [NSURL URLWithString:@"http://imgur.com/y9MIaCS.png"]];
+    [self setImageRandom];
+    NSURL *myURL = [NSURL URLWithString:@"http://imgur.com/2vQtZBb.png"];
+    NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession *mySession = [NSURLSession sessionWithConfiguration:config];
+    NSURLSessionDownloadTask *dlt = [mySession downloadTaskWithURL:myURL completionHandler:^(NSURL *_Nullable location, NSURLResponse *_Nullable response, NSError *_Nullable error) {
+        if (error) {
+            NSLog(@"error: %@", error.localizedDescription);
+            return;
+        }
+        
+        NSData *data = [NSData dataWithContentsOfURL:location];
+        UIImage *image = [UIImage imageWithData:data];
+        
+        [[NSOperationQueue mainQueue] addOperationWithBlock: ^{
+            self.iPhoneImageView.image = image;
+        }];
+        
+    }];
+    [dlt resume];
+    
+    }
+
+- (IBAction)setImage:(id)sender {
+    [self setImageRandom];
 }
-
-
+    
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+- (void) setImageRandom {
+    NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession *mySession = [NSURLSession sessionWithConfiguration:config];
+    NSURLSessionDownloadTask *dlt = [mySession downloadTaskWithURL:self.phoneURLArray[arc4random_uniform(5)] completionHandler:^(NSURL *_Nullable location, NSURLResponse *_Nullable response, NSError *_Nullable error) {
+        if (error) {
+  //          NSLog(@"error: %@", error.localizedDescription);
+            return;
+        }
+        
+        NSData *data = [NSData dataWithContentsOfURL:location];
+        UIImage *image = [UIImage imageWithData:data];
+        
+        [[NSOperationQueue mainQueue] addOperationWithBlock: ^{
+            self.iPhoneImageView.image = image;
+        }];
+        
+    }];
+    [dlt resume];
+    
 
+}
 @end
